99548a778d487465ced22495369eb139662eb80f
jherreraO <jherrera@revoktek.com>
2025-08-11 15:23:17 -0600

    feat(project) First commit creation project

---
+++ src/main/java/com/revoktek/timbrado/TimbradoApplication.java
@@ -0,0 +1,13 @@
+package com.revoktek.timbrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimbradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimbradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoktek/timbrado/TimbradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoktek.timbrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimbradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
